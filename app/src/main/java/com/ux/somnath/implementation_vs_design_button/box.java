6f5c3ca5bc600932003d5e6edd507a4d2896aa56
Somnath Nabajja <uxgraphy@gmail.com>
2019-07-24 18:06:00 +0530

    Button experiments done

---
+++ app/src/main/java/com/ux/somnath/implementation_vs_design_button/box.java
@@ -0,0 +1,13 @@
+package com.ux.somnath.implementation_vs_design_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class box extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_box);
+    }
+}
